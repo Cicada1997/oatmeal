74752c7efe68503604eb4e48a47ea16359479c1d
cCi97 <francis.backstrom@gmail.com>
2025-07-28 23:17:05 +0200

    moved ECS to separate file and made header file

--- src/main.cc
+++ src/main.cc
@@ -4,74 +4,14 @@
 #include <unordered_set>
 
 #include "sparse_set.hh"
+#include "ecs.hh"
 
 // HALELELUJA
 // int x = 9;
 // fmt::print("Apa nr {}", x);
 
-enum class ComponentType {
-    Transform,
-    Physics,
-    Render,
-    Player,
-};
-
-typedef struct {
-    uint64_t id;
-    std::unordered_set<ComponentType> components;
-} Entity;
-
-class ECS
-{
-    sparse_set<std::unordered_set<ComponentType>> World;
-
-    public:
-    ECS(uint64_t MAX_ENTITIES)
-        : World(MAX_ENTITIES)
-    {
-        fmt::print("ECS initialized");
-    };
-
-    void create_entity(uint64_t entityID, std::unordered_set<ComponentType> components) 
-    {
-        World.add(entityID, components);
-    };
-
-    std::unordered_set<ComponentType> getEntity(uint64_t entityID) 
-    {
-        return *World.get(entityID);
-    };
-
-    // @brief Gets the entity from the world, then appends the
-    // @brief enum class representing the component to the world.
-    void AddComponent(uint64_t entityID, ComponentType component)
-    {
-        auto entityComponents = *World.get(entityID);
-        entityComponents.insert(component);
-    };
-
-    // @brief Gets the entity from the world, then deletes the
-    // @brief enum class representing the component from the world.
-    void withdrawComponent(uint64_t entityID, ComponentType component)
-    {
-        auto entityComponents = *World.get(entityID);
-        entityComponents.erase(component);
-    };
-
-    void removeEntity(uint64_t entityID)
-    {
-        World.remove(entityID);
-    }
-};
-
-struct Vector
-{
-    double x;
-    double y;
-};
-
+struct Vector { double x; double y; };
 double dabs(double x) { return x >= 0 ? x : x*-1; }; // @brief calculates absolute value for doubles.
-
 double vecDist(Vector pos1, Vector pos2)
 {
     double dx2 = dabs(pos1.x - pos2.x) * dabs(pos1.x - pos2.x);
@@ -88,7 +28,7 @@ void test_sparse()
 
     double dist = vecDist(*transform.get(12), *transform.get(2));
 
-    fmt::print("distance: {}", dist);
+    fmt::print("distance: {}\n", dist);
 };
 
 void test_ESC()
